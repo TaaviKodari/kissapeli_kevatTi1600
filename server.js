c1e44b8f53db71016710ca49bd18765bc27e79ee
kodarit11 <taavi.makela@gmail.com>
2020-03-31 16:58:51 +0300

    heroku muutokseet aloitettu

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
-app.listen(3000,()=> console.log("kuuntelen"));
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=> console.log("kuuntelen"));
 app.use(express.static("public"));
+app.use(express.json({limit: '1mb'});
