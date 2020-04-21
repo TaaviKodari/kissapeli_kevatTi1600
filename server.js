1d0e3a1ca4131bcf96dbca63681ef19955fe3549
kodarit11 <taavi.makela@gmail.com>
2020-04-21 16:27:31 +0300

    server.js korjaus

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=> console.log("kuuntelen"));
+app.listen(port,()=> console.log("kuuntelen porttia " + port));
+
 app.use(express.static("public"));
-app.use(express.json({limit: '1mb'});
+app.use(express.json({limit: '1mb'}));
